d5cb3cce91a4034d1531ae7f4a4694e3a1bb0938
nguyentanphatt <bloodomega97@gmail.com>
2024-10-28 18:53:24 +0700

    push project

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
