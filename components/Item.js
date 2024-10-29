f11548bc941fcdcbca2a6c64f9912dff84dbdd6b
nguyentanphatt <bloodomega97@gmail.com>
2024-10-29 18:12:24 +0700

    completed main page

---
+++ components/Item.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Text, View, Image, StyleSheet} from 'react-native'
+import star from '../assets/icon/star.png'
+const Item = ( {image, name, rating, price} ) => {
+  return (
+    <View style={styles.container}>
+        <Image source={image} style={{width: 100, height: 100}}/>
+        <Text style={{fontWeight: '700'}}>{name}</Text>
+        <View style={styles.info}>
+            <View style={styles.info}>
+                <Image source={star} style={{width: 15, height: 15}}/>
+                <Text>{rating}</Text>
+            </View>
+            <Text style={{color: '#0F89D1', fontWeight: '600'}}>${price}</Text>
+        </View>
+    </View>
+  )
+}
+
+export default Item
+
+const styles=StyleSheet.create({
+    container:{
+        backgroundColor: '#ededed',
+        padding: 5
+    },
+    info:{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+})
--- page/Home.js
+++ page/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, Image, ScrollView, Dimensions, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, View, Image, ScrollView, Dimensions, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native'
 import Swiper from 'react-native-swiper'
 import Category from '../components/Category'
 
@@ -8,7 +8,12 @@ import sneaker_category from '../assets/image/sneaker_category.png'
 import lipstick from '../assets/image/lipstick.png'
 import fruit_category from '../assets/image/fruit_category.png'
 import banner from '../assets/image/banner.png'
-
+import bags from '../assets/image/bags.jpg'
+import macbook from '../assets/image/macbook.jpg'
+import sneaker_1 from '../assets/image/sneaker_1.png'
+import tablet_1 from '../assets/image/tablet_1.png'
+import pear from '../assets/image/pear.png'
+import Item from '../components/Item'
 const Home = () => {
 
   const category = [
@@ -30,12 +35,56 @@ const Home = () => {
     },
   ]
 
-  const itemsPerPage = 3
-  const screenWidth = Dimensions.get('window').width;
+  const product = [
+    {
+      name: 'Shoes',
+      rating: '4.5',
+      price: '299',
+      image: sneaker_1
+    },
+    {
+      name: 'Tablet',
+      rating: '4.5',
+      price: '499',
+      image: tablet_1
+    },
+    {
+      name: 'Pear',
+      rating: '4.5',
+      price: '59',
+      image: pear
+    },
+    {
+      name: 'Macbook',
+      rating: '4.5',
+      price: '599',
+      image: macbook
+    },
+    {
+      name: 'Bags',
+      rating: '4.5',
+      price: '99',
+      image: bags
+    },
+    {
+      name: 'Sneaker',
+      rating: '4.5',
+      price: '159',
+      image: banner
+    },
+  ]
 
-  const pages = [];
+
+  const itemsPerPage = 3
+
+  const pagesForCategory = [];
   for (let i = 0; i < category.length; i += itemsPerPage) {
-    pages.push(category.slice(i, i + itemsPerPage));
+    pagesForCategory.push(category.slice(i, i + itemsPerPage));
+  }
+
+  const pagesForProduct = [];
+  for (let i = 0; i < product.length; i += itemsPerPage) {
+    pagesForProduct.push(product.slice(i, i + itemsPerPage));
   }
 
   return (
@@ -48,12 +97,12 @@ const Home = () => {
           autoplay={false}
           style={{height: 150}}
         >
-        {pages.map((page, index)=>(
+        {pagesForCategory.map((page, index)=>(
           <View key={index} style={{ flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 20 }}>
           {page.map((item) => (
             <Category image={item.image} title={item.title}/>
           ))}
-        </View>
+          </View>
         ))}
         </Swiper>
         <View style={styles.banner}>
@@ -68,6 +117,36 @@ const Home = () => {
             <Image source={banner} style={{width: 200, height: 100}}/>
           </View>
         </View>
+        <View style={styles.sales}>
+          <View>
+            <ImageBackground source={bags} style={{width: 180, height: 150}}/>
+            <Text style={{position: 'absolute', top: 20, width: 40, paddingHorizontal: 5, borderTopRightRadius: 10, borderBottomRightRadius: 10, backgroundColor: '#ff4c4c', color: '#fff'}}>30%</Text>
+          </View>
+          <View>
+            <ImageBackground source={macbook} style={{width: 180, height: 150}}/>
+            <Text style={{position: 'absolute', top: 20, width: 40, paddingHorizontal: 5, borderTopRightRadius: 10, borderBottomRightRadius: 10, backgroundColor: '#ffb732', color: '#fff'}}>30%</Text>
+          </View>
+        </View>
+        <View style={styles.recommend}>
+          <Text style={{fontWeight: '700', fontSize: 16}}>Recommended for you</Text>
+          <Text style={{color: 'gray'}}>View all</Text>
+        </View>
+        <Swiper
+          loop={false}
+          showsPagination={false}
+          showsButtons={false}
+          autoplay={false}
+          style={{height: 200}}
+        >
+          {pagesForProduct.map((page, index)=>(
+          <View key={index} style={{ flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 20 }}>
+          {page.map((item) => (
+            <Item image={item.image} name={item.name} rating={item.rating} price={item.price}/>
+          ))}
+          </View>
+        ))}
+        </Swiper>
+
       </View>
     </ScrollView>
   )
@@ -92,6 +171,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#000',
     borderRadius: 5,
+    marginTop: 10
+  },
+  sales:{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 10
+  },
+  recommend:{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     marginTop: 10
   }
 })
